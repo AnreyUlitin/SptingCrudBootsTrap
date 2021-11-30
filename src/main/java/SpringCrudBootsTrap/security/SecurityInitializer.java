f29589b933abe9c4a8fef8ff092905e994b37da2
Andrey <anulitin@rambler.ru>
2021-11-30 19:25:33 +0300

    SpringCrudBootsTrap

--- src/main/java/SpringCrudBootsTrap/security/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package SpringCrudBootsTrap.security;
-
-import org.springframework.security.web.context.*;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-    //пустой класс, использующийся для регистрации модуля в спринг-контейнере
-}
